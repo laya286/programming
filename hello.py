db842da096b51d044be7c40fd59b416c99ffec88
laya286 <lathikasris286@gmail.com>
2025-07-31 14:45:34 +0530

    Create hello.py
    hello

---
+++ hello.py
@@ -0,0 +1,8 @@
+hello.py
+pin=input("what you want")
+if pin=="withdraw":
+  print("Take the amount")
+  amount=int(input("How much money you want"))
+  print("Take the cash"),amount
+else:
+    print("Deposite your money")
