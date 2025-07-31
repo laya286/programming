1df210eba24f7198a9c3408a2e401242392ebc7c
laya286 <lathikasris286@gmail.com>
2025-07-31 14:48:14 +0530

    Create calculation.py
    calculation

---
+++ calculation.py
@@ -0,0 +1,8 @@
+input calculation.py
+pin=input("what you want")
+if pin=="withdraw":
+  print("Take the amount")
+  amount=int(input("How much money you want"))
+  print("Take the cash"),amount
+else:
+    print("Deposite your money")
