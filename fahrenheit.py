a07921f4ec5e5fe19005a116a4aa8440fbb83598
laya286 <lathikasris286@gmail.com>
2025-07-31 14:33:51 +0530

    Create fahrenheit.py
    fahrenheit

---
+++ fahrenheit.py
@@ -0,0 +1,8 @@
+Farenheit.txt
+Centigrade to farenheit
+Algorithm
+Step 1: start the execution
+Step 2: Read the value of celsius
+Step 3: calculate farenheit=(1.8*celsius+32)
+Step 4: print the value of fahenheit
+  
