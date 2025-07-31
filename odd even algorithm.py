1f71544f0b2ba177f58a2bfc42f0718e46c6100d
laya286 <lathikasris286@gmail.com>
2025-07-31 14:31:35 +0530

    Create odd even algorithm.py
    algorithm

---
+++ odd even algorithm.py
@@ -0,0 +1,9 @@
+Algorithm.txt
+Odd or even
+Algorithm:
+Step 1: start the program
+Step 2: Read the value a
+Step 3: if a%2==0 then
+Step 4: print the value is even
+Step 5: otherwise the value is odd
+Step 6: stop the execution.
