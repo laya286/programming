b36e23eaf30e754d01d928c84b9dd131d35c5dd7
laya286 <lathikasris286@gmail.com>
2025-07-31 14:29:37 +0530

    Create atm.py
    money

---
+++ atm.py
@@ -0,0 +1,7 @@
+pin=input("what you want")
+if pin=="withdraw":
+  print("Take the amount")
+  amount=int(input("How much money you want"))
+  print("Take the cash"),amount
+else:
+    print("Deposite your money")
